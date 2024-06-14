579b4c67fcc137ee81f0f34f94fb12a9bce325e5
rafaelfs <rafaelfelix.santos@gmail.com>
2024-06-15 07:37:03 +0800

    fix: adjusting manifest service

--- app/src/Core/Manifest/Services/ManifestService.ts
+++ app/src/Core/Manifest/Services/ManifestService.ts
@@ -1,12 +1,8 @@
-import { ManifestDto } from '../Dto/ManifestDto';
-import packageJson from '../../../package.json';
-import DateFormat from '../Utils/DateFormat';
 
 export class ManifestService {
-  public async invoke(): Promise<ManifestDto> {
+  public async invoke(): Promise<any> {
     return {
-      timestamp: DateFormat.timestamp(new Date()),
-      version: packageJson.version,
+      result: 'ok'
     };
   }
 }
